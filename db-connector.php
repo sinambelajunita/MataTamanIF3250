3f7cc3491d197bf59a8396ee4564bc344bd68d94
fahmidumadi <fah.midum.adi@gmail.com>
2015-02-12 18:20:03 +0700

    ganti db-connector

--- db-connector.php
+++ db-connector.php
@@ -1,9 +1,7 @@
-<?php 
-	$mysql_hostname = "localhost";
-	$mysql_user = "root";
-	$mysql_password = "";
-	$mysql_database = "db_matataman";
-	$prefix = "";
-	$db = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Oops something went wrong");
-	mysql_select_db($mysql_database, $db) or die("Something wrong.");
+<?php
+	$con=mysqli_connect("localhost","root","","db_matataman");
+	// Check connection
+	if (mysqli_connect_errno()) {
+  		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
 ?>
